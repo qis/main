13a312216232aec811f170f96c7a84981bf2056d
Alexej Harm <alexej.h@xiphos.de>
2018-05-17 14:36:54 +0200

    testing beast

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,58 @@
-#if 0
+// git clone git@github.com:qis/bcrypt ${VCPKG}/ports/bcrypt
+// git clone git@github.com:qis/compat ${VCPKG}/ports/compat
+// git clone git@github.com:qis/sql ${VCPKG}/ports/sql
+// vcpkg install benchmark boost-beast date fmt gtest jsoncpp openssl utfcpp bcrypt compat sql
+
 #include <config.h>
-#include <ice/async.h>
-#include <ice/net/tcp/socket.h>
-#include <ice/scope.h>
-#include <array>
-#include <exception>
-#include <iostream>
-#include <string_view>
+#include <boost/asio/experimental/co_spawn.hpp>
+#include <boost/asio/experimental/detached.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/signal_set.hpp>
+#include <boost/asio/write.hpp>
+#include <cstdio>
 
-constexpr std::string_view g_response =
-  "HTTP/1.1 200 OK\r\n"
-  "Date: Thu, 19 Apr 2018 04:34:52 GMT\r\n"
-  "Connection: keep-alive\r\n"
-  "Server: ice/0.1.0\r\n"
-  "Content-Type: text/html; charset=utf-8\r\n"
-  "Content-Length: 0\r\n"
-  "\r\n";
+using boost::asio::ip::tcp;
+using boost::asio::experimental::co_spawn;
+using boost::asio::experimental::detached;
+namespace this_coro = boost::asio::experimental::this_coro;
 
-ice::task respond(ice::net::tcp::socket& client, ice::async_mutex& mutex, std::string_view data) {
-  const auto lock = co_await mutex.scoped_lock_async();
-  const auto sent = co_await client.send(data.data(), data.size());
-  if (sent != data.size()) {
-    std::cerr << "could not send response to " << client << " (" << sent << "/" << data.size() << ")" << std::endl;
-  }
-  co_return;
-}
+template <typename T>
+using awaitable = boost::asio::experimental::awaitable<T, boost::asio::io_context::executor_type>;
 
-ice::task handle(ice::net::tcp::socket client) {
-  client.set(ice::net::option::no_delay(true));
-  std::array<char, 1024> buffer;
-  ice::async_mutex mutex;
-  bool newline = false;
-  while (true) {
-    const auto size = co_await client.recv(buffer.data(), buffer.size());
-    if (!size) {
-      break;
-    }
-    for (std::size_t i = 0; i < size; i++) {
-      switch (buffer[i]) {
-      case '\r': break;
-      case '\n':
-        if (newline) {
-          respond(client, mutex, g_response);
-          newline = false;
-        } else {
-          newline = true;
-        }
-        break;
-      default: newline = false; break;
-      }
+awaitable<void> echo(tcp::socket socket) {
+  auto token = co_await this_coro::token();
+  try {
+    char data[1024];
+    while (true) {
+      std::size_t n = co_await socket.async_read_some(boost::asio::buffer(data), token);
+      co_await async_write(socket, boost::asio::buffer(data, n), token);
     }
   }
-  co_await mutex.lock_async();  // wait until all send operations finish
-  co_return;
+  catch (std::exception& e) {
+    std::fprintf(stderr, "error: %s\n", e.what());
+  }
 }
 
-ice::task server(ice::context& context) {
-  const auto se = ice::on_scope_exit([&]() { context.stop(); });
-  ice::net::endpoint endpoint("127.0.0.1", 8080);
-  ice::net::tcp::socket socket(context, endpoint.family());
-  socket.set(ice::net::option::reuse_address(true));
-  socket.bind(endpoint);
-  socket.listen();
-  std::cout << PROJECT << ' ' << VERSION << ' ' << COMPANY << ' ' << COPYING << std::endl;
+awaitable<void> listener() {
+  auto executor = co_await this_coro::executor();
+  auto token = co_await this_coro::token();
+  tcp::acceptor acceptor(executor.context(), { tcp::v4(), 8080 });
   while (true) {
-    handle(co_await socket.accept());
+    tcp::socket socket = co_await acceptor.async_accept(token);
+    co_spawn(executor, [socket = std::move(socket)]() mutable { return echo(std::move(socket)); }, detached);
   }
-  co_return;
 }
 
 int main() {
   try {
-    ice::context context;
-    server(context);
-    context.run();
+    boost::asio::io_context io_context(1);
+    boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
+    signals.async_wait([&](auto, auto) { io_context.stop(); });
+    co_spawn(io_context, listener, detached);
+    io_context.run();
   }
-  catch (const std::exception& e) {
-    std::cerr << e.what() << std::endl;
+  catch (std::exception& e) {
+    std::fprintf(stderr, "critical error: %s\n", e.what());
   }
-}
-#endif
-
-#include <date/tz.h>
-#include <chrono>
-#include <filesystem>
-#include <iostream>
-
-int main() {
-  auto path = std::filesystem::current_path();
-  while (true) {
-    if (std::filesystem::is_directory(path / "res" / "tzdata") || path == path.parent_path()) {
-      break;
-    }
-    path = path.parent_path();
-  }
-  date::set_install((path / "res" / "tzdata").u8string());
-  const auto time = date::make_zoned(date::locate_zone("Europe/Berlin"), std::chrono::system_clock::now());
-  std::cout << time << std::endl;
 }
