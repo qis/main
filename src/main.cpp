6422a09ac87f8f62bd690dced743d3661c9e69d8
Alexej Harm <alexej.h@xiphos.de>
2018-05-18 09:08:03 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -6,40 +6,52 @@
 #include <config.h>
 #include <boost/asio/experimental/co_spawn.hpp>
 #include <boost/asio/experimental/detached.hpp>
+#include <boost/asio/experimental/redirect_error.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/signal_set.hpp>
 #include <boost/asio/write.hpp>
+#include <array>
 #include <cstdio>
 
 using boost::asio::ip::tcp;
 using boost::asio::experimental::co_spawn;
 using boost::asio::experimental::detached;
+using boost::asio::experimental::redirect_error;
 namespace this_coro = boost::asio::experimental::this_coro;
 
 template <typename T>
 using awaitable = boost::asio::experimental::awaitable<T, boost::asio::io_context::executor_type>;
 
 awaitable<void> echo(tcp::socket socket) {
-  auto token = co_await this_coro::token();
-  try {
-    char data[1024];
-    while (true) {
-      std::size_t n = co_await socket.async_read_some(boost::asio::buffer(data), token);
-      co_await async_write(socket, boost::asio::buffer(data, n), token);
+  boost::system::error_code ec;
+  auto token = redirect_error(co_await this_coro::token(), ec);
+  std::array<char, 4096> data;
+  while (true) {
+    std::size_t size = co_await socket.async_read_some(boost::asio::buffer(data), token);
+    if (ec) {
+      std::fprintf(stderr, "read error: %s\n", ec.message().data());
+      break;
     }
-  }
-  catch (std::exception& e) {
-    std::fprintf(stderr, "error: %s\n", e.what());
+    co_await async_write(socket, boost::asio::buffer(data, size), token);
+    if (ec) {
+      std::fprintf(stderr, "write error: %s\n", ec.message().data());
+      break;
+    }
   }
 }
 
 awaitable<void> listener() {
+  boost::system::error_code ec;
+  auto token = redirect_error(co_await this_coro::token(), ec);
   auto executor = co_await this_coro::executor();
-  auto token = co_await this_coro::token();
   tcp::acceptor acceptor(executor.context(), { tcp::v4(), 8080 });
   while (true) {
-    tcp::socket socket = co_await acceptor.async_accept(token);
+    auto socket = co_await acceptor.async_accept(token);
+    if (ec) {
+      std::fprintf(stderr, "accept error: %s\n", ec.message().data());
+      continue;
+    }
     co_spawn(executor, [socket = std::move(socket)]() mutable { return echo(std::move(socket)); }, detached);
   }
 }
