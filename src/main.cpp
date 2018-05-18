39dfea79ebb86628257a45f39c7f04bcc909beee
Alexej Harm <alexej.h@xiphos.de>
2018-05-18 17:12:09 +0200

    working on beast

--- src/main.cpp
+++ src/main.cpp
@@ -4,64 +4,91 @@
 // vcpkg install benchmark boost-beast date fmt gtest jsoncpp openssl utfcpp bcrypt compat sql
 
 #include <config.h>
+
+#include <boost/asio.hpp>
 #include <boost/asio/experimental/co_spawn.hpp>
 #include <boost/asio/experimental/detached.hpp>
 #include <boost/asio/experimental/redirect_error.hpp>
-#include <boost/asio/io_context.hpp>
-#include <boost/asio/ip/tcp.hpp>
-#include <boost/asio/signal_set.hpp>
-#include <boost/asio/write.hpp>
-#include <array>
+#include <boost/beast/http.hpp>
+#include <boost/beast/core/flat_buffer.hpp>
+
+namespace net {
+
+using namespace boost::asio;
+namespace http = boost::beast::http;
+namespace this_coro = experimental::this_coro;
+
+using boost::system::error_code;
+
+using boost::beast::flat_buffer;
+
+using experimental::co_spawn;
+using experimental::detached;
+using experimental::redirect_error;
+using experimental::awaitable;
+
+}  // namespace net
+
+#include <cstdlib>
+#include <utility>
 #include <cstdio>
 
-using boost::asio::ip::tcp;
-using boost::asio::experimental::co_spawn;
-using boost::asio::experimental::detached;
-using boost::asio::experimental::redirect_error;
-namespace this_coro = boost::asio::experimental::this_coro;
+template <typename T>
+using awaitable = net::awaitable<T, net::io_context::executor_type>;
 
 template <typename T>
-using awaitable = boost::asio::experimental::awaitable<T, boost::asio::io_context::executor_type>;
+using strand = net::awaitable<T, net::strand<net::io_context::executor_type>>;
 
-awaitable<void> echo(tcp::socket socket) {
-  boost::system::error_code ec;
-  auto token = redirect_error(co_await this_coro::token(), ec);
-  std::array<char, 4096> data;
+awaitable<void> session(net::ip::tcp::socket socket) {
+  net::error_code ec;
+  auto token = net::redirect_error(co_await net::this_coro::token(), ec);
+
+  net::flat_buffer buffer;
+  net::http::request<net::http::string_body> request;
+
   while (true) {
-    std::size_t size = co_await socket.async_read_some(boost::asio::buffer(data), token);
+    (void)co_await net::http::async_read(socket, buffer, request, token);
     if (ec) {
-      std::fprintf(stderr, "read error: %s\n", ec.message().data());
+      std::fprintf(stderr, "request error: %s\n", ec.message().data());
       break;
     }
-    co_await async_write(socket, boost::asio::buffer(data, size), token);
+    auto response = net::http::response<net::http::string_body>(net::http::status::ok, request.version());
+    response.body() = "OK\n";
+    response.prepare_payload();
+    (void)co_await net::http::async_write(socket, response, token);
     if (ec) {
-      std::fprintf(stderr, "write error: %s\n", ec.message().data());
+      std::fprintf(stderr, "response error: %s\n", ec.message().data());
       break;
     }
   }
+  socket.shutdown(net::ip::tcp::socket::shutdown_both);
+  socket.close();
 }
 
 awaitable<void> listener() {
-  boost::system::error_code ec;
-  auto token = redirect_error(co_await this_coro::token(), ec);
-  auto executor = co_await this_coro::executor();
-  tcp::acceptor acceptor(executor.context(), { tcp::v4(), 8080 });
+  net::error_code ec;
+  auto token = net::redirect_error(co_await net::this_coro::token(), ec);
+  auto executor = co_await net::this_coro::executor();
+  net::ip::tcp::acceptor acceptor(executor.context(), { net::ip::tcp::v4(), 8080 });
+  acceptor.set_option(net::socket_base::reuse_address(true));
   while (true) {
     auto socket = co_await acceptor.async_accept(token);
     if (ec) {
       std::fprintf(stderr, "accept error: %s\n", ec.message().data());
       continue;
     }
-    co_spawn(executor, [socket = std::move(socket)]() mutable { return echo(std::move(socket)); }, detached);
+    net::co_spawn(executor, [socket = std::move(socket)]() mutable {
+      return session(std::move(socket));
+    }, net::detached);
   }
 }
 
 int main() {
   try {
-    boost::asio::io_context io_context(1);
-    boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
+    net::io_context io_context(1);
+    net::signal_set signals(io_context, SIGINT, SIGTERM);
     signals.async_wait([&](auto, auto) { io_context.stop(); });
-    co_spawn(io_context, listener, detached);
+    net::co_spawn(io_context, listener, net::detached);
     io_context.run();
   }
   catch (std::exception& e) {
